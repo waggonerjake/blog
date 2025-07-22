1e282ad6ef348fdb8f975c7f80150f4493f6e271
Jake Waggoner <waggonerjake99@gmail.com>
2025-07-21 22:48:28 -0400

    Add content to about page

---
+++ src/config.ts
@@ -0,0 +1,21 @@
+export const SITE = {
+  website: "https://jakewaggoner.com/", // replace this with your deployed domain
+  author: "Jake Waggoner",
+  profile: "https://jakewaggoner.com/",
+  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
+  title: "Jake Waggoner",
+  ogImage: "astropaper-og.jpg",
+  lightAndDarkMode: true,
+  postPerIndex: 4,
+  postPerPage: 4,
+  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+  showArchives: false,
+  showBackButton: true, // show back button in post detail
+  editPost: {
+    enabled: false,
+  },
+  dynamicOgImage: true,
+  dir: "ltr", // "rtl" | "auto"
+  lang: "en", // html lang code. Set this empty and default will be "en"
+  timezone: "America/Indianapolis", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+} as const;
