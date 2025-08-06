b5e26310730ab8bbf5d5de6003536a963228e5f0
Jake Waggoner <waggonerjake99@gmail.com>
2025-08-06 17:46:10 -0400

    Try again

--- api/get-word.ts
+++ api/get-word.ts
@@ -1,4 +1,4 @@
-async function GET(request: Request) {
+export const GET = async (request: Request) => {
   const response = await fetch('https://scs7xuuadxsagxft.public.blob.vercel-storage.com/words.json');
   const products = await response.json();
   return Response.json(products);
