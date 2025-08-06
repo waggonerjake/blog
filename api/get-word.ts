9052e6438d9ea7c8c22793e7cc6162a74d852f70
Jake Waggoner <waggonerjake99@gmail.com>
2025-08-06 17:49:02 -0400

    Again

--- api/get-word.ts
+++
@@ -1,5 +0,0 @@
-export const GET = async (request: Request) => {
-  const response = await fetch('https://scs7xuuadxsagxft.public.blob.vercel-storage.com/words.json');
-  const products = await response.json();
-  return Response.json(products);
-}
